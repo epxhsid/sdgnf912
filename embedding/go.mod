ff06b0f0633e045e3bca4d1a919cf93436ba8dc8
Joaquin M. <joaqu26@proton.me>
2026-09-14 00:22:54 +0800

    load text embedder and install a tokenizer dependency

--- embedding/go.mod
+++ embedding/go.mod
@@ -2,4 +2,12 @@ module github.com/epxhsid/embedding
 
 go 1.26.5
 
-require github.com/yalue/onnxruntime_go v1.36.0 // indirect
+require (
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/schollz/progressbar/v2 v2.15.0 // indirect
+	github.com/sugarme/regexpset v0.0.0-20200920021344-4d4ec8eaf93c // indirect
+	github.com/sugarme/tokenizer v0.3.0 // indirect
+	github.com/yalue/onnxruntime_go v1.36.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
+)
--- embedding/main.go
+++ embedding/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	ort "github.com/yalue/onnxruntime_go"
 )
@@ -9,11 +10,48 @@ import (
 func main() {
 	ort.SetSharedLibraryPath("./onnxruntime/lib/libonnxruntime.so")
 
-	err := ort.InitializeEnvironment()
-	if err != nil {
-		panic(err)
+	if err := ort.InitializeEnvironment(); err != nil {
+		log.Fatal(err)
 	}
 	defer ort.DestroyEnvironment()
 
-	fmt.Println("ONNX Runtime initialized successfully")
+	session, err := ort.NewDynamicAdvancedSession(
+		"./models/bge-small-en-v1.5/model.onnx",
+		nil,
+		nil,
+		nil,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer session.Destroy()
+
+	fmt.Println("ONNX model loaded successfully")
+
+	inputs, outputs, err := ort.GetInputOutputInfo(
+		"./models/bge-small-en-v1.5/model.onnx",
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("INPUTS:")
+	for _, input := range inputs {
+		fmt.Printf(
+			"  name=%s dimensions=%v type=%v\n",
+			input.Name,
+			input.Dimensions,
+			input.DataType,
+		)
+	}
+
+	fmt.Println("OUTPUTS:")
+	for _, output := range outputs {
+		fmt.Printf(
+			"  name=%s dimensions=%v type=%v\n",
+			output.Name,
+			output.Dimensions,
+			output.DataType,
+		)
+	}
 }
